231bc5547cf930089aad12731ff356ea2f97e0af
unknown <bernasconivargas.mateo@gmail.com>
2023-04-09 09:38:29 -0600

    added proxy and wsgi settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,13 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1$tqw+8^ef6v6wh2a$q(yxdlobu8b@pr3!z$!z6l1_hus&f!13'
+SECRET_KEY = os.environ.get('DJANGO_SCECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(',')
+    )
+)
 
 # Application definition
 
